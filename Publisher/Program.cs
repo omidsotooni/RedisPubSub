57bb79f08e6afce10bf4c31eb1c1539a90c8870f
Omid Sotooni <mr.sotooni@yahoo.com>
2023-07-10 14:07:44 +0330

    Add project files.

---
+++ Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+namespace Publisher
+{
+    class Program
+    {
+        private const string RedisConStr = "127.0.0.1:6379,syncTimeout=10000,DefaultDatabase=2,allowAdmin=True,connectTimeout=3000,ssl=False,abortConnect=False,connectRetry=10";
+        private static ConnectionMultiplexer _connection = ConnectionMultiplexer.Connect(RedisConStr);
+        private const string ChannelName = "asp-net-core-channel";
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Console Publisher";
+
+            var pubsub = _connection.GetSubscriber();
+
+            RedisChannel myChannel = RedisChannel.Literal(ChannelName);
+
+            pubsub.PublishAsync(myChannel, "This is a test message!", CommandFlags.FireAndForget);
+            
+            Console.WriteLine($"Message Successfully sent to {ChannelName}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+namespace Subscriber
+{
+    class Program
+    {
+        private const string RedisConStr = "127.0.0.1:6379,syncTimeout=10000,DefaultDatabase=2,allowAdmin=True,connectTimeout=3000,ssl=False,abortConnect=False,connectRetry=10";
+        private static ConnectionMultiplexer _connection = ConnectionMultiplexer.Connect(RedisConStr);
+        private const string ChannelName = "asp-net-core-channel";
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Console Subscriber";
+
+            Console.WriteLine($"Listening {ChannelName}");
+
+            var pubsub = _connection.GetSubscriber();
+
+            RedisChannel myChannel = RedisChannel.Literal(ChannelName);
+
+            pubsub.Subscribe(myChannel, (channel, message) => Console.WriteLine($"Message received from {ChannelName} : " + message));
+
+            Console.ReadLine();
+        }
+    }
+}
